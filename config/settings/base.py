0dda1b6ee88cf90df6c027316d33af471ba4217f
Teddy Coder <teddy_coder@mail.ru>
2019-07-02 09:40:33 +0700

    Добавлено главное приложение

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "mega_analitika.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    'mega_analitika.main.apps.MainConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
