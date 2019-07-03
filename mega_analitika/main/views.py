f69e53d967142d23da078a7dff326d70f6d6f3af
Teddy Coder <teddy_coder@mail.ru>
2019-07-03 23:55:05 +0700

    Сделаны графики

--- mega_analitika/main/views.py
+++ mega_analitika/main/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 from django.contrib import messages
 
 from .forms import EnterTaskForm
 from .functions import execute_task
+from .models import Task, PopularDay, PopularTime
 
 
 class MainView(View):
@@ -32,4 +33,25 @@ class MainView(View):
 
 class ReportView(View):
     def get(self, request):
-        return render(request, 'main/report.html')
+
+        if request.GET['task']:
+            task_pk = request.GET['task']
+        else:
+            return render(request, 'main/empty.html')
+
+        task = get_object_or_404(Task, pk = task_pk)
+        pds = PopularDay.objects.filter(task=task).all()
+        dic_days = {pd.day: pd.counter for pd in pds}
+        lst_days = [ dic_days.get(i, 0) for i in range(1, 8)]
+
+        pts = PopularTime.objects.filter(task=task).all()
+        dic_time = {pt.time: pt.counter for pt in pts}
+        lst_time = [ dic_days.get(i, 0) for i in range(24)]
+
+        context = {
+            'title': 'Это ваш отчет!',
+            'description': task.description,
+            'data_days': lst_days,
+            'data_time': lst_time,
+        }
+        return render(request, 'main/report.html', context)
