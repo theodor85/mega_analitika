a66a822a2c0eb18607db310fff2e50af9e7f64b2
Teddy Coder <teddy_coder@mail.ru>
2019-07-03 13:01:52 +0700

    Сделана валидация ссылки на раздел сайта

--- mega_analitika/main/forms.py
+++ mega_analitika/main/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from django.forms.widgets import EmailInput, URLInput, TextInput
+from django.core import validators
 
 class EnterTaskForm(forms.Form):
     ''' Форма для ввода задания '''
     url = forms.URLField(label='URL', 
         help_text='Ссылка на раздел сайта www.olx.ua',
-        widget = URLInput(attrs={'class':'form-control'}))
+        widget = URLInput(attrs={'class':'form-control'}),
+        validators = [ validators.RegexValidator( r'^https://www.olx.ua/[a-z\-]{1,}/[a-z\-]{1,}/' ) ],
+        error_messages = {'invalid': 'Нужно указать ссылку на существующий раздел сайта olx.ua!'}
+        )
 
     email = forms.EmailField(label='E-mail', 
         help_text='Ваш E-mail, на который будет выслана ссылка с отчетом',
@@ -14,8 +18,3 @@ class EnterTaskForm(forms.Form):
     description = forms.CharField(max_length=250, label='Описание',
         help_text='Это описание будет использовано в заголовке отчёта',
         widget = TextInput(attrs={'class':'form-control'}))
-
-    # TODO: 
-    # сделатб валидатор поля url, с проверкой, что:
-    # а. это ссылка на olx.ua
-    # б. это ссылка на существующий раздел
